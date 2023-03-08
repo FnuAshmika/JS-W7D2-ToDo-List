d791195a4963599fd85c1a344330a83dd2b718ea
Fnu Ashmika <ashmika.bv@gmail.com>
2023-03-07 19:00:35 -0600

    frist commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const todoList = [
+    {
+        id:1,
+      title: "Groceries",
+      description: "Milk, bread, eggs"
+    },
+    {
+        id:2,
+        title: "Homework",
+        description: "Complete assigned homework for JavaScript"
+      },
+      {
+        id:3,
+        title: "Trash",
+        description: "Take out the trash."
+      },
+    {
+        id:4,
+      title: "Do laundry",
+      description: "Wash clothes and towels"
+    },
+    {
+        id:5,
+      title: "Study",
+      description: "Watch class recordings again."
+    }
+  ]
+
+  const todoContainer = document.getElementById("list")
+  for (const task of todoList ){
+    todoContainer.innerHTML += `
+    <li class="card" id=${task.id}>
+    <h3>${task.title}</h3>
+    <p>${task.description}</p>   
+    <button class="btn btn-primary done-button">&#10004;</button>
+</li>
+
+    `
+  }
+  for (const task of todoList ){
+    const taskid = document.getElementById(task.id)
+    taskid.addEventListener('click', (event) =>{
+        if(event.target.classList.contains('done-button')){
+        taskid.remove()
+        }
+    })
+  }
